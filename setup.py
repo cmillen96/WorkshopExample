520f6e08771e55b14f23e1946a7d8e185be64455
Curtis Millen <curtizpls@gmail.com>
2017-12-01 15:22:28 +1000

    changed names

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(name='WorkshopExample',
       version='0.0.1',
       description='Random example project for coding workshop',
-      url='http://github.com/Samreay/WorkshopExample',
-      author='Samuel Hinton',
-      author_email='samuelreay@gmail.com',
+      url='http://github.com/cmillen96/WorkshopExample',
+      author='Curtis Millen',
+      author_email='citrus.melon96@gmail.com',
       license='MIT',
       install_requires=['numpy'],
       packages=find_packages(exclude=('tests', 'doc', 'data'))
